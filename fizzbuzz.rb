c7bf0df07aa23de51b3970ce65906ac1826f04a0
Mahbubur Rahman <mail@mahbub.me>
2018-12-26 06:21:54 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,9 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz( number )
-  if ( 0 == number % 3 )
+  if 0 == number % 3
     return "Fizz"
+  elseif 0 == number % 5
+    return ""
   end
 end
